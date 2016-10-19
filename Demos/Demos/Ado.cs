ab9c6f06b439bbeeb78fd45cfe9703969048af5a
sunkaixuan <610262374@qq.com>
2016-10-20 03:16:09 +0800

    ok

--- Demos/Demos/Ado.cs
+++ Demos/Demos/Ado.cs
@@ -5,6 +5,7 @@ using System.Text;
 using NewTest.Dao;
 using Models;
 using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
 
 namespace NewTest.Demos
 {
@@ -18,18 +19,18 @@ namespace NewTest.Demos
             using (var db = SugarDao.GetInstance())
             {
                 var r1 = db.GetDataTable("select * from student");
-                var r2 = db.GetSingle<Student>("select top 1 * from student");
+                var r2 = db.GetSingle<Student>("select  * from student LIMIT 0,1");
                 var r3 = db.GetScalar("select  count(1) from student");
                 var r4 = db.GetReader("select  count(1) from student");
                 r4.Dispose();
-                var r5 = db.GetString("select  top 1 name from student");
+                var r5 = db.GetString("select   name from student LIMIT 0,1");
                 var r6 = db.ExecuteCommand("select 1");
 
 
                 //参数支持
                 var p1 = db.GetDataTable("select * from student where id=@id", new {id=1 });
                 var p2 = db.GetDataTable("select * from student where id=@id", new Dictionary<string, object>() { { "id", "1" } });//目前只支持 Dictionary<string, object>和Dictionary<string, string>
-                var p3 = db.GetDataTable("select * from student where id=@id", new SqlParameter("@id",1) );
+                var p3 = db.GetDataTable("select * from student where id=@id", new MySqlParameter("@id",1) );
 
             }
         }
