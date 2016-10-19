2b2fe5900dcb84b0cbc4a5b94cb92e002ba126ae
sunkaixuan <610262374@qq.com>
2016-10-20 03:47:11 +0800

    ok

--- SqlSugar/Tool/SqlSugarToolWithSqlMethod.cs
+++ SqlSugar/Tool/SqlSugarToolWithSqlMethod.cs
@@ -233,30 +233,15 @@ namespace MySqlSugar
 
         internal static string GetTtableColumnsInfo(string tableName)
         {
-            string sql = @"SELECT  Sysobjects.name AS TABLE_NAME ,
-								syscolumns.Id  AS TABLE_ID,
-								syscolumns.name AS COLUMN_NAME ,
-								systypes.name AS DATA_TYPE ,
-								syscolumns.length AS CHARACTER_MAXIMUM_LENGTH ,
-								sys.extended_properties.[value] AS COLUMN_DESCRIPTION ,
-								syscomments.text AS COLUMN_DEFAULT ,
-								syscolumns.isnullable AS IS_NULLABLE,
-                                (case when exists(SELECT 1 FROM sysobjects where xtype= 'PK' and name in ( 
-                                SELECT name FROM sysindexes WHERE indid in( 
-                                SELECT indid FROM sysindexkeys WHERE id = syscolumns.id AND colid=syscolumns.colid 
-                                ))) then 1 else 0 end) as IS_PRIMARYKEY
-
-								FROM    syscolumns
-								INNER JOIN systypes ON syscolumns.xtype = systypes.xtype
-								LEFT JOIN sysobjects ON syscolumns.id = sysobjects.id
-								LEFT OUTER JOIN sys.extended_properties ON ( sys.extended_properties.minor_id = syscolumns.colid
-																			 AND sys.extended_properties.major_id = syscolumns.id
-																		   )
-								LEFT OUTER JOIN syscomments ON syscolumns.cdefault = syscomments.id
-								WHERE   syscolumns.id IN ( SELECT   id
-												   FROM     SYSOBJECTS
-												   WHERE    xtype in( 'U','V') )
-								AND ( systypes.name <> 'sysname' ) AND Sysobjects.name='" + tableName + "'  AND systypes.name<>'geometry' AND systypes.name<>'geography'  ORDER BY syscolumns.colid";
+            string sql = @"SELECT
+                                    TABLE_NAME, 
+                                    column_name AS COLUMN_NAME,
+                                    is_nullable AS `IS_NULLABLE`,
+                                    column_default  AS  `COLUMN_DEFAULT`,
+                                    column_comment  AS  `COLUMN_DESCRIPTION`
+                                    FROM
+                                    Information_schema.columns where TABLE_NAME='"+tableName+@"' ORDER BY TABLE_NAME
+                                      ";
             return sql;
         }
 
