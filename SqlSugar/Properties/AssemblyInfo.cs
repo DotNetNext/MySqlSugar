c3da1bd9e2d853e48535bbbcaba061fc86dace46
sunkaixuan <610262374@qq.com>
2016-10-23 00:10:33 +0800

    ok

--- SqlSugar/Properties/AssemblyInfo.cs
+++ SqlSugar/Properties/AssemblyInfo.cs
@@ -33,4 +33,3 @@ using System.Runtime.InteropServices;
 // 方法是按如下所示使用“*”:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.1,1,2")]
-[assembly: AssemblyFileVersion("3.1.1.2")]
