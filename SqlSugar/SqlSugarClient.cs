f85870c67762a0fe476cc3303736f704dae0243d
sunkaixuan <610262374@qq.com>
2016-10-30 07:07:35 +0800

    3.1.1.7

--- SqlSugar/SqlSugarClient.cs
+++ SqlSugar/SqlSugarClient.cs
@@ -843,7 +843,7 @@ namespace MySqlSugar
                     }
                     else if (underType == SqlSugarTool.DateType)
                     {
-                        objValue = "'" + objValue.ToString() + "'";
+                        objValue = "'" + objValue.ObjToDate().ToString("yyyy-MM-dd HH:mm:ss") + "'";
                     }
                     else if (underType == SqlSugarTool.BoolType)
                     {
